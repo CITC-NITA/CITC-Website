4dacc737ae0eb09a6002e3d5e3c55870b482302d
vivekanandram9 <98640582+vivekanandram9@users.noreply.github.com>
2024-05-25 10:18:40 +0530

    updated blog page

--- pages/contactPage/contact.js
+++ pages/contactPage/contact.js
@@ -1,3 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click" , () =>{
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+}))
+
+
+
+
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
