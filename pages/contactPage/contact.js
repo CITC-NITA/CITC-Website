c70b166b0eed4220d15ef14e6e23777361da5d9f
vivekanandram9 <98640582+vivekanandram9@users.noreply.github.com>
2024-05-28 00:12:19 +0530

    blog and contact page updated

--- pages/contactPage/contact.js
+++ pages/contactPage/contact.js
@@ -15,7 +15,7 @@ document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click" ,
 
 
 /*contact section*/
-const wrapper = document.querySelector(".wrapper");
+/*const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
@@ -95,4 +95,41 @@ carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-wrapper.addEventListener("mouseleave", autoPlay);
+wrapper.addEventListener("mouseleave", autoPlay);*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 3, // Minimal delay to create continuous effect
+      disableOnInteraction: false, // Continue autoplay after user interaction
+    },
+    speed: 8000, // Speed of transition in milliseconds
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
+
