08cffe14434f35710afadd1e5757c8be7a138322
vivekanandram9 <98640582+vivekanandram9@users.noreply.github.com>
2024-06-03 21:50:26 +0530

    updated navbar ,footer,contact page and blog page

---
+++ pages/eventPage/event.js
@@ -0,0 +1,36 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+const logo = document.querySelector(".logo");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+function myFunction(x) {
+    if (x.matches) {
+      
+      document.querySelector(".logo img").setAttribute("src","../images/citc.png")
+    }
+    else{
+        document.querySelector(".logo img").setAttribute("src","../images/logo.png")
+    }
+}
+  
+  
+  var x = window.matchMedia("(max-width: 576px)");
+
+  myFunction(x);
+
+  x.addEventListener("change", function () {
+    myFunction(x);
+  });
